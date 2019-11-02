9b8b87dae9b0867a69177b73a7a55167a39e4a0a
Katsuki <Katsuki_Sato0130@icloud.com>
2019-11-02 17:22:31 +0900

    Add sign out

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,7 +1,13 @@
 import React from "react";
+import SignOut from "./SignOut";
 
 function Admin(props) {
-  return <div>Admin Page</div>;
+  return (
+    <div>
+      <h2>Admin Page </h2>
+      <SignOut />
+    </div>
+  );
 }
 
 export default Admin;

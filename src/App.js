e09f48412e3e7e58126acb43cc198f0cf33d2034
Katsuki <Katsuki_Sato0130@icloud.com>
2019-11-02 10:56:43 +0900

    Add private route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 import "./App.css";
+import { AuthContext } from "./context/auth";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home Page</Link>
-          </li>
-          <li>
-            <Link to="/admin">Admin Page</Link>
-          </li>
-        </ul>
-        <Route exact path="/" component={Home} />
-        <Route path="/admin" component={Admin} />
-      </div>
-    </Router>
+    <AuthContext.Provider value={false}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin Page</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <PrivateRoute path="/admin" component={Admin} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
